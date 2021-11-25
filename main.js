8099532094662820b2bc555a054568f3eff63bde
Ivan Jankov <ivanjankov07@gmail.com>
2021-11-25 10:49:02 +0100

    Additional project pages added

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+	// TOGGLE DARK MODE
 	const checkbox = document.getElementById('checkbox');
 	const mainNav = document.getElementById('main-menu');
+	const aboutMe = document.getElementById('about-me');
+	const contact = document.getElementById('contact');
+
 	checkbox.addEventListener('change', () => {
 		document.body.classList.toggle('light');
+		aboutMe.classList.toggle('light');
+		contact.classList.toggle('light');
 		mainNav.classList.toggle('light');
 	});
 });
---
+++ scripts/lile-portfolio.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+	// SLIDER FUNCTIONALITY
+
+	const sliderImages = Array.from(document.querySelectorAll('.slider-img'));
+	const sliderCircles = Array.from(document.querySelectorAll('.slider-circle'));
+	const rightArrowSlider = document.getElementById('right-arrow-slider');
+	const leftArrowSlider = document.getElementById('left-arrow-slider');
+	index = 0;
+	rightArrowSlider.addEventListener('click', () => {
+		index++;
+		if (index > sliderImages.length - 1) {
+			index = 0;
+		}
+		changeImage(sliderImages, sliderCircles);
+	});
+	leftArrowSlider.addEventListener('click', () => {
+		index--;
+
+		if (index < 0) {
+			index = sliderImages.length - 1;
+		}
+		changeImage(sliderImages, sliderCircles);
+	});
+
+	function changeImage(array, circle) {
+		for (let i = 0; i < array.length; i++) {
+			if (i !== index) {
+				array[i].classList.add('img-hidden');
+				circle[i].classList.remove('circle-filled');
+			} else {
+				array[i].classList.remove('img-hidden');
+				circle[index].classList.add('circle-filled');
+			}
+		}
+	}
+});
